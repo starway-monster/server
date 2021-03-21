1c8954b8efe5275d7cdf6b1f9df13b3d38a9dc9b
Raumo0 <dharapko@gmail.com>
2021-03-21 12:43:08 +0300

    Dijkstra algorithm added

---
+++ src/main/java/monster/starway/server/data/entities/Graph.java
@@ -0,0 +1,20 @@
+package monster.starway.server.data.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Graph {
+    private Set<Node> nodes = new HashSet<>();
+
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Set<Node> nodes) {
+        this.nodes = nodes;
+    }
+}
--- src/main/java/monster/starway/server/services/WayService.java
+++ src/main/java/monster/starway/server/services/WayService.java
@@ -1,5 +1,8 @@
 package monster.starway.server.services;
 
+import monster.starway.server.data.entities.Dijkstra;
+import monster.starway.server.data.entities.Graph;
+import monster.starway.server.data.entities.Node;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,6 +10,45 @@ import java.util.List;
 @Service
 public class WayService {
     public String getFilteredWays(String from, String to, List<String> excludedZones) {
+        String result = getEchoMessage(from, to, excludedZones) + "\n" + getWay();
+        return result;
+    }
+
+    private String getWay() {
+        Node nodeA = new Node("A");
+        Node nodeB = new Node("B");
+        Node nodeC = new Node("C");
+
+        nodeA.addDestination(nodeB, 10);
+        nodeB.addDestination(nodeA, 10);
+        nodeB.addDestination(nodeC, 20);
+        nodeC.addDestination(nodeB, 20);
+
+        Graph graph = new Graph();
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+
+        Dijkstra.calculateShortestPathFromSource(graph, nodeA);
+
+        String aToB = "";
+        for (Node node : nodeB.getShortestPath()) {
+            aToB += node.getName() + "--->";
+        }
+        aToB.substring(0, aToB.length() - 5);
+        aToB += "B: " + nodeB.getDistance();
+
+        String aToC = "";
+        for (Node node : nodeC.getShortestPath()) {
+            aToC += node.getName() + "--->";
+        }
+        aToC.substring(0, aToC.length() - 5);
+        aToC += "C: " + nodeC.getDistance();
+
+        return aToB + "\n" + aToC;
+    }
+
+    private String getEchoMessage(String from, String to, List<String> excludedZones) {
         String excluded = "";
         if (excludedZones != null && excludedZones.size() > 0) {
             excluded = ", excluded:";
@@ -15,6 +57,6 @@ public class WayService {
             }
             excluded = excluded.substring(0, excluded.length()-2);
         }
-        return String.format("Hello from: " +  from + ", to:" + to + excluded);
+        return String.format("Hello from: " + from + ", to:" + to + excluded);
     }
 }
