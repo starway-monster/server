dff957f74617056de503858210468de1ed69b6bc
Raumo0 <dharapko@gmail.com>
2021-03-22 04:29:46 +0300

    Stubs removed

--- src/main/java/monster/starway/server/services/WayService.java
+++ src/main/java/monster/starway/server/services/WayService.java
@@ -36,9 +36,7 @@ public class WayService {
 
         return searchDTO;
     }
-
-
-
+    
     private Set<Node> transformChannelsToNodes(List<Channel> channels) {
         Map<String, Node> nodes = getNodesFromChannels(channels);
         initiateNodesDirections(nodes, channels);
@@ -95,44 +93,6 @@ public class WayService {
             }
 //            todo: throw exception
         }
-    }
-
-    private SearchDTO getWay() {
-        Node nodeA = new Node("A");
-        Node nodeB = new Node("B");
-        Node nodeC = new Node("C");
-
-        nodeA.addDestination(nodeB, 10);
-        nodeB.addDestination(nodeA, 10);
-        nodeB.addDestination(nodeC, 20);
-        nodeC.addDestination(nodeB, 20);
-
-        List<Node> nodes = new ArrayList<>(){{
-            add(nodeA);
-            add(nodeB);
-            add(nodeC);
-        }};
-        Graph graph = getZonesGraph(nodes);
-
-        Dijkstra.calculateShortestPathFromSource(graph, nodeA);
-
-        List<Node> pathByTransfers = nodeB.getShortestPath();
-        pathByTransfers.add(nodeB);
-        List<Node> pathByFee = nodeC.getShortestPath();
-        pathByFee.add(nodeC);
-
-        PathDTO pathDTOByTransfers = transformToPathDTO(pathByTransfers, nodeB.getDistance());
-        PathDTO pathDTOByFee = transformToPathDTO(pathByFee, nodeC.getDistance());
-
-        return new SearchDTO(pathDTOByTransfers, pathDTOByFee);
-    }
-
-    private Graph getZonesGraph(List<Node> nodes) {
-        Graph graph = new Graph();
-        for (Node node : nodes) {
-            graph.addNode(node);
-        }
-        return graph;
     }
 
     public PathDTO transformToPathDTO(List<Node> nodes, int fee) {
